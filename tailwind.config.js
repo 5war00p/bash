6bba4a048fad4c254635926a862b2f8e492e33e5
5war00p <maddisaiswaroop@gmail.com>
2023-07-03 13:51:33 +0530

    initial commit with header

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#000080',
+        green: '#55FF55'
+      },
+      backgroundColor: {
+        aubergine: {
+          light: '#77216F',
+          mid: '#5E2750',
+          dark: '#2C001E'
+        },
+      },
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
