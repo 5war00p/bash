79111eb6630925fac8fe4e90540b23c44879ba54
5war00p <maddisaiswaroop@gmail.com>
2023-07-15 18:44:22 +0530

    fix: styling & dynamic data loading in education

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -2,9 +2,9 @@
 	import { contactDetails } from 'data'
 </script>
 
-<div class="my-20 mx-80 grid grid-cols-1 gap-4 sm:grid-cols-3">
-	{#each contactDetails as {icon, title, value}, index (index)}
-	{@const Icon = icon}
+<div class="m-20 flex flex-col space-y-4">
+	{#each contactDetails as { icon, title, value }, index (index)}
+		{@const Icon = icon}
 		<div
 			class="relative flex items-center space-x-5 rounded-lg border border-slate-100 px-3 py-4 hover:border-slate-400"
 		>
@@ -13,9 +13,8 @@
 			</div>
 			<div class="min-w-0 flex-1">
 				<a href={'#'} target="_blank" class="focus:outline-none text-white">
-					<span class="absolute inset-0" aria-hidden="true" />
-					<p class="text-2xl font-semibold">{title}</p>
-					<p class="text-xl font-medium">{value}</p>
+					<p class="text-2xl font-semibold truncate">{title}</p>
+					<p class="text-xl font-medium truncate">{value}</p>
 				</a>
 			</div>
 		</div>
---
+++ src/lib/data/academics.ts
@@ -0,0 +1,19 @@
+const academics = [
+	{
+		title: "Graduation (Bachelor's)",
+		institute: 'Rajiv Gandhi University of Knowledge Technologies, Nuzvid',
+		duration: '2018-2022'
+	},
+	{
+		title: 'High School (Intermediate)',
+		institute: 'Rajiv Gandhi University of Knowledge Technologies, Nuzvid',
+		duration: '2016-2018'
+	},
+	{
+		title: 'Secondary School (SSC)',
+		institute: 'Government High School, Pathapatnam',
+		duration: '2015-2016'
+	}
+]
+
+export default academics
