c1d4467fa5069c6e8a606328438e28391659df78
5war00p <maddisaiswaroop@gmail.com>
2025-05-24 19:13:10 +0530

    feat: add projects section

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,7 +1,8 @@
 import about from './about'
 import academics from './academics'
 import contactDetails from './contact'
+import projects from './projects'
 import socialProfiles from './social'
 import work from './work'
 
-export { academics, about, contactDetails, socialProfiles, work }
+export { academics, about, contactDetails, socialProfiles, work, projects }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,19 +6,22 @@
 	import Academics from 'components/Academics.svelte'
 	import About from 'components/About.svelte'
 	import Help from 'components/Help.svelte'
+	import Projects from 'components/Projects.svelte'
 	import Work from 'components/Work.svelte'
 </script>
 
-{#if $query === COMMANDS.SOCIAL}
-	<Social />
+{#if $query === COMMANDS.ACADEMICS}
+	<Academics />
 {:else if $query === COMMANDS.ABOUT}
 	<About />
 {:else if $query === COMMANDS.CONTACT}
 	<Contact />
-{:else if $query === COMMANDS.ACADEMICS}
-	<Academics />
 {:else if $query === COMMANDS.HELP}
 	<Help />
+{:else if $query === COMMANDS.PROJECTS}
+	<Projects />
+{:else if $query === COMMANDS.SOCIAL}
+	<Social />
 {:else if $query === COMMANDS.WORK}
 	<Work />
 {:else if $query === COMMANDS.EMPTY || $query === COMMANDS.ROOT}
