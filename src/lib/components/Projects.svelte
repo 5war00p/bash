43746b494749d1beb86e2edee10492f3f59f4311
5war00p <maddisaiswaroop@gmail.com>
2025-05-24 19:12:29 +0530

    feat: add projects section

---
+++ src/lib/components/Projects.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { projects } from 'data'
+</script>
+
+<div class="mx-auto py-10 w-11/12 sm:w-5/6">
+	<ul role="list" class="space-y-16">
+		{#each projects as { icon, title, description, skills }, index (index)}
+			<li class="relative flex gap-x-4">
+				<div
+					class="flex-auto text-gray-500 space-y-3 sm:space-y-4 py-1.5 sm:py-1"
+				>
+					<div class="flex items-center gap-4">
+						<div class="h-6 w-6 rounded-full bg-gray-200 ring-1 ring-gray-400">
+							<div class="flex items-center justify-center h-full w-full">
+								{icon}
+							</div>
+						</div>
+						<p
+							class="font-medium text-red-700 truncate col-start-1 row-start-1 text-lg sm:text-xl"
+						>
+							{title}
+						</p>
+					</div>
+					<p class="text-base sm:text-lg">{description}</p>
+					<div class="flex flex-wrap gap-1">
+						{#each skills as skill}
+							<span
+								class="inline-flex items-center rounded-full bg-gray-50 px-1.5 py-0.5 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+								>{skill}</span
+							>
+						{/each}
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</div>
---
+++ src/lib/data/projects.ts
@@ -0,0 +1,33 @@
+const projects = [
+	{
+		icon: '👁️',
+		title: 'Eye Gaze Controlled Web Apps',
+		description: `Using the webgazer.js library, developed three applications controlled by eye-gaze: swiping images left and right, scrolling articles/pages up and down, and a game where the player shoots aliens using eye movement as the shooter. These projects demonstrate innovative user interaction through gaze tracking.`,
+		skills: ['JavaScript', 'WebGazer.js', 'HTML', 'CSS']
+	},
+	{
+		icon: '📄',
+		title: 'Subtitle Meaning Finder',
+		description: `Developed a tool to enhance language learning through movies by analyzing subtitle (.srt) files. The application identifies words in the subtitles and displays their meanings based on the user's English proficiency level, which is set at the start. The system operates in real-time, allowing users to watch movies while instantly accessing word definitions, eliminating the need to pause and manually search for meanings. This project integrates natural language processing and user experience design to facilitate immersive learning.`,
+		skills: [
+			'Python',
+			'Media Player Integration',
+			'Word Classification',
+			'Natural Language Processing (NLP)'
+		]
+	},
+	{
+		icon: '🤖',
+		title: 'Robotics Projects',
+		description: `Worked on multiple robotics projects, including building manual robots, line followers, soccer-playing robots, mobile-controlled robots, and drones. These projects involved designing and assembling hardware, programming microcontrollers (such as Arduino and NodeMCU), and integrating various modules like DTMF, Bluetooth, and wireless communication. Gained hands-on experience in robotics, automation, and embedded systems.`,
+		skills: [
+			'Arduino',
+			'NodeMCU',
+			'DTMF Module',
+			'Bluetooth Module',
+			'Robotics'
+		]
+	}
+]
+
+export default projects
